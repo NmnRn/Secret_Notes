07da78d007bbaa02244456bb182e7e733d5fed52
Numan Eren Erdem <numanerenerdem7@hotmail.com>
2025-12-10 15:46:04 +0300

    bugs fixed

--- main.py
+++ main.py
@@ -28,6 +28,9 @@ my_list = []
 dictionary = dict()
 
 def sync():
+    global my_list, dictionary
+    my_list.clear()
+    dictionary.clear()
     with open("text.txt", "r", encoding="utf-8") as f:
 
         for line in f.readlines():
@@ -38,23 +41,31 @@ def sync():
             my_list.append(line)
 
         x = 0
+        turning = 0
+
         if not len(my_list) == 0:
-            while x <= len(my_list) / 2:
-                dictionary.update({my_list[x]: my_list[x + 1]})
+            while x < len(my_list):
+                dictionary.update({my_list[turning*2]: my_list[turning*2 + 1]})
                 x += 2
+                turning += 1
+                print(len(my_list), x)
+        f.close()
+
 sync()
 
 def save_note():
     with open("text.txt","a", encoding="utf-8") as file:
         msg_entry1 = entry1.get().strip()
         msg_entry2 = entry2.get().strip()
+
         if text1.get("1.0",tk.END).strip() == "":
             messagebox.showerror("Error", "Please enter a secret")
             return
         if msg_entry1 in dictionary.keys():
             return messagebox.showerror("Error", "Please enter another title")
-
-        msg_text = text1.get("1.0", tk.END).strip() + msg_entry2
+        if "_" in msg_entry2:
+            return messagebox.showerror("Error", "Your password must not contain “_”")
+        msg_text = text1.get("1.0", tk.END).strip() + "_" +msg_entry2 + "_"
 
         base64Bytes = base64.b64encode(msg_text.encode("utf-8"))
         base64Bytes_decode = base64Bytes.decode("utf-8")
@@ -62,31 +73,37 @@ def save_note():
         if msg_entry1 and msg_entry2 and msg_text:
             file.write(f"\n{msg_entry1}\n{base64Bytes_decode}\n")
             messagebox.showinfo(title="Info", message="Secret Saved")
+            file.close()
             entry1.delete("0", tk.END)
             entry2.delete("0", tk.END)
             text1.delete("1.0", tk.END)
+            dictionary.update({msg_entry1: base64Bytes_decode})
 
         else:
             messagebox.showwarning(title="Warning", message="Please Enter All Information's.")
-
-        file.close()
+            file.close()
 
 
 def get_secret():
-        sync()
-        try:
-            byte64 = dictionary[entry1.get().strip()].encode("utf-8")
 
-            secret_note_byte = base64.b64decode(byte64)
-            secret_note = secret_note_byte.decode("utf-8")
+    try:
+        if entry1.get().strip() == "": return messagebox.showerror("Error", "Please Enter Your Title")
+        if entry2.get().strip() == "": return messagebox.showerror("Error", "Please Enter Your Master Key")
 
-            if secret_note.endswith(entry2.get().strip()):
-                note = secret_note.replace(entry2.get().strip(), "")
-                text1.delete("1.0", tk.END)
-                text1.insert(tk.INSERT, note)
-        except Exception as e:
-            messagebox.showerror(title="Error", message=f"Error: Not Found")
+        byte64 = dictionary[entry1.get().strip()].encode("utf-8")
+        secret_note_byte = base64.b64decode(byte64)
 
+        secret_note = secret_note_byte.decode("utf-8")
+
+        if secret_note.endswith("_"+entry2.get().strip()+"_"):
+            note = secret_note.replace("_"+entry2.get().strip()+"_", "")
+            text1.delete("1.0", tk.END)
+            text1.insert(tk.INSERT, note)
+
+        else:
+            messagebox.showerror(title="Error", message="Not Found")
+    except Exception as e:
+        messagebox.showerror(title="Error", message="Not Found")
 
 
 image = Image.open("secret.png")
